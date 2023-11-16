012623e34012427745c6a8253eff812dfe28136d
oksanamatviyiv <113504985+oksanamatviyiv@users.noreply.github.com>
2023-11-17 00:11:58 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,36 +4,34 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('.feedback-form');
   const emailInput = form.querySelector('input[name="email"]');
   const messageInput = form.querySelector('textarea[name="message"]');
-  const formData = { email: '', message: '' };
 
-  // Function to save and log form data
-  const saveAndLogData = () => {
-    formData.email = emailInput.value;
-    formData.message = messageInput.value;
+  const saveAndLogFormData = () => {
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
     console.log('Form data:', formData);
-  
   };
-  const throttledSaveAndLogData = throttle(saveAndLogData, 500);
 
-  // Load saved form data from localStorage
-  const savedFormData = localStorage.getItem('feedback-form-state');
-  if (savedFormData) {
-    const parsedData = JSON.parse(savedFormData);
-    emailInput.value = parsedData.email || '';
-    messageInput.value = parsedData.message || '';
+  const throttledSaveAndLogFormData = throttle(saveAndLogFormData, 500);
+
+  emailInput.addEventListener('input', throttledSaveAndLogFormData);
+  messageInput.addEventListener('input', throttledSaveAndLogFormData);
+
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email || '';
+    messageInput.value = formData.message || '';
   }
 
-  // Attach event listeners to form fields
-  emailInput.addEventListener('input', throttledSaveAndLogData);
-  messageInput.addEventListener('input', throttledSaveAndLogData);
-
-  // Handle form submission
   form.addEventListener('submit', event => {
     event.preventDefault();
-    console.log('Form submitted with data:', formData);
-
-    // Clear form fields and localStorage
+    console.log('Form submitted with data:', {
+      email: emailInput.value,
+      message: messageInput.value,
+    });
     emailInput.value = '';
     messageInput.value = '';
     localStorage.removeItem('feedback-form-state');
